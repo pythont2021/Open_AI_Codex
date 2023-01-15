c3ffe5e68ec89f92300a79fa09ee7638a55f73f2
pythont2021 <pythont2021@gmail.com>
2023-01-15 06:54:59 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from the server -> bot's responce
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://openai-app-8mbh.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
